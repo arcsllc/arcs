cc927855a47f5e95289f724229259b133a88308b
mtg1993 <87932999+mtg1993@users.noreply.github.com>
2021-07-28 11:12:21 +0300

    Update __manifest__.py

--- retained_amount/__manifest__.py
+++ retained_amount/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock', 'product','web','sale','point_of_sale'],
+    'depends': ['accounting','purchase'],
 
     # always loaded
     'data': [
