3939dfed3b89c016b3d6438c7bde7d0000b2d231
mtg1993 <87932999+mtg1993@users.noreply.github.com>
2021-07-28 10:52:04 +0300

    Update account_move.py

--- retained_amount/models/account_move.py
+++ retained_amount/models/account_move.py
@@ -16,7 +16,7 @@ class Madfox_AccountMove(models.Model):
         res = super(Madfox_AccountMove, self).create(vals)
         if res.move_type=='in_invoice' and res.invoice_line_ids.purchase_order_id and res.invoice_line_ids.purchase_order_id.requisition_id.retained_percentage>0:
             retained_amount = res.amount_total * (res.invoice_line_ids.purchase_order_id.requisition_id.retained_percentage/100)
-            account = self.env['account.account'].search([('code', '=', 230101)])
+            account = self.env['account.account'].search([('code', '=', '230101')])
             rec = {
                 'partner_id': res.partner_id,
                 'quantity': '1',
