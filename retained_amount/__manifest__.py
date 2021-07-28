2d5a567b473b121b4dcf17913b1819b237c62797
mtg1993 <87932999+mtg1993@users.noreply.github.com>
2021-07-28 12:14:18 +0300

    Update __manifest__.py

--- retained_amount/__manifest__.py
+++ retained_amount/__manifest__.py
@@ -1,18 +1,40 @@
 # -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Madfox Solutions
+#
+#    Copyright (C) 2021-TODAY Madfox Solutions(<https://www.madfox.solutions>).
+#    Author: Mohamad MOaiad Bashiti (moaiad@madfox.solutions)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
 {
     'name': "retained_amount",
     'version': '14.0.1.0.0',
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        arcs modifications for purchase agreement""",
 
     'description': """
-        Long description of module's purpose
+        add percentge to purchase agreement
+        add retained amount
+        generate tax for purchase agreement
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "MadFox limited",
+    'website': "http://www.madfox.solutions",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
