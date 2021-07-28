eca017104524cab51ddd745052f9db7de383c327
mmbashiti <mmbashiti@gmail.com>
2021-07-28 15:30:35 +0300

    Update __manifest__.py

--- retained_amount/__manifest__.py
+++ retained_amount/__manifest__.py
@@ -43,7 +43,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'accounting'],
+    'depends': ['base', 'purchase'],
 
     # always loaded
     'data': [
