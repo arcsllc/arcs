edc74d59f41a790aa14a3b9d833e7a5ec27f177e
mtg1993 <87932999+mtg1993@users.noreply.github.com>
2021-07-28 10:55:26 +0300

    Add files via upload

--- retained_amount/models/account_move.py
+++ retained_amount/models/account_move.py
@@ -17,13 +17,22 @@ class Madfox_AccountMove(models.Model):
         if res.move_type=='in_invoice' and res.invoice_line_ids.purchase_order_id and res.invoice_line_ids.purchase_order_id.requisition_id.retained_percentage>0:
             retained_amount = res.amount_total * (res.invoice_line_ids.purchase_order_id.requisition_id.retained_percentage/100)
             account = self.env['account.account'].search([('code', '=', '230101')])
-            rec = {
-                'partner_id': res.partner_id,
-                'quantity': '1',
-                'name': 'Retained Amount',
-                'price_unit': retained_amount*-1,
-                'account_id': account.id
-            }
+            if account:
+                rec = {
+                    'partner_id': res.partner_id,
+                    'quantity': '1',
+                    'name': 'Retained Amount',
+                    'price_unit': retained_amount*-1,
+                    'account_id': account.id
+                }
+            else:
+                rec = {
+                    'partner_id': res.partner_id,
+                    'quantity': '1',
+                    'name': 'Retained Amount',
+                    'price_unit': retained_amount * -1,
+                    
+                }
             res.invoice_line_ids=[(0,0,rec)]
 
         return res
