01559d41cf020104356958eff7c3940534192777
mmbashiti <mmbashiti@gmail.com>
2021-08-29 09:55:58 +0300

    Add files via upload

--- retained_amount/models/Madfox_PurchaseRequisition.py
+++ retained_amount/models/Madfox_PurchaseRequisition.py
@@ -9,6 +9,23 @@ class MadfoxPurchaseRequisition(models.Model):
     retained_percentage = fields.Float(string="retain percentage")
     payment_term_id = fields.Many2one(comodel_name='account.payment.term', string="Payment Terms")
     name = fields.Char(string='Reference', required=True, copy=False, default='New', readonly=False)
+    total_retained_amount = fields.Integer(compute='_compute_retained_amount', string='Number of Orders')
+
+    @api.depends('purchase_ids')
+    def _compute_retained_amount(self):
+        for requisition in self:
+            requisition.total_retained_amount = requisition.compute_total_retained_amount()
+
+    @api.model
+    def compute_total_retained_amount(self):
+        total = 0
+        for po in self.purchase_ids:
+            for invoice in po.invoice_ids:
+                if invoice.state == 'posted':
+                    for line in invoice.invoice_line_ids:
+                        if line.account_id and line.account_id.code == '230101':
+                            total += line.price_unit
+        return total
 
     def action_in_progress(self):
         lines = []
@@ -28,25 +45,25 @@ class MadfoxPurchaseRequisition(models.Model):
             for taxRule in taxRules.children_tax_ids:
                 if (i == 1):
                     # the stamp rules
-                    stampValue = 2*(taxRule.amount / 100) * totalAmount
+                    stampValue = (taxRule.amount / 100) * totalAmount
 
                     taxs.append([stampValue, taxRule.name])
                     lines.append([0, False, {'account_id': account.id, 'name': taxRule.name,
-                                             'credit': 0, 'debit': taxs[0][0], 'partner_id': 21}])
+                                             'credit': 0, 'debit': taxs[0][0], 'partner_id': 21, 'quantity':2}])
                 elif i == 2:
-                    taxs.append([2*(taxRule.amount / 100) * stampValue, taxRule.name])
+                    taxs.append([(taxRule.amount / 100) * stampValue, taxRule.name])
 
                     account = self.env['account.account'].search([('code', '=', '230704')])
                     lines.append([0, False, {'account_id': account.id, 'name': taxRule.name,
                                              'credit': 0, 'debit': taxs[1][0],
-                                             'partner_id': 21}])
+                                             'partner_id': 21, 'quantity':2}])
 
                 else:
-                    taxs.append([2*(taxRule.amount / 100) * stampValue, taxRule.name])
+                    taxs.append([(taxRule.amount / 100) * stampValue, taxRule.name])
 
                     account = self.env['account.account'].search([('code', '=', '230705')])
                     lines.append([0, False, {'account_id': account.id, 'name': taxRule.name,
-                                             'credit': 0, 'debit': taxs[2][0], 'partner_id': 21}])
+                                             'credit': 0, 'debit': taxs[2][0], 'partner_id': 21, 'quantity':2}])
 
                 i = i + 1
         # create account move entre
@@ -55,9 +72,9 @@ class MadfoxPurchaseRequisition(models.Model):
         # raise exceptions.ValidationError('total='+str(total)+', stamp='+str(stampValue)+', tax1='+str(taxs[1][0])+', tax2='+str(taxs[2][0])+', taxtotal='+str((stampValue+taxs[1][0]+taxs[2][0])))
         lines.append([0, False, {'name': 'ضريبة عقد' + ' (' + self.user_id.display_name + ' ' + self.name + ')',
                                  'debit': 0, 'credit': (taxs[0][0] + taxs[1][0] + taxs[2][0]), 'partner_id': 1,
-                                 'account_id': account.id}])
+                                 'account_id': account.id, 'quantity':2}])
         self.env['account.move'].create({
-            'name': 'رسوم تعاقد' + self.name, 'ref': self.vendor_id.display_name+'-'+self.name, 'requisition_id': self.id,
+            'name': 'purchase agreement taxes', 'ref': 'purchase agreement', 'requisition_id': self.id,
             'journal_id': 3, 'line_ids': lines
         })
 
